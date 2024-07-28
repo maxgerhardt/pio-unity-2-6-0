93b0cfa906dc67d095faa35f053f6c9dced85ecc
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2024-07-28 14:47:31 +0200

    Fix build

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#ifdef ARDUINO
 #include <Arduino.h>
 
 void setup() {
@@ -5,5 +6,10 @@ void setup() {
 }
 
 void loop() {
-    
+
 }
+#else
+int main(int argc, char* argv[]) {
+    return 0;
+}
+#endif
